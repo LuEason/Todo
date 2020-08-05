7b7129860dbe088a709714f4ca3aa4b1adc7fe52
LuEason <chenzscd@163.com>
2020-08-05 17:05:01 +0800

    [Eason]feature : first init

---
+++ src/main/java/com/OOCL/Todo/model/Todo.java
@@ -0,0 +1,10 @@
+package com.OOCL.Todo.model;
+
+import lombok.Data;
+
+@Data
+public class Todo {
+    private Integer id;
+    private String text;
+    private String Status;
+}
---
+++ src/main/java/com/OOCL/Todo/service/serviceImpl/TodoServiceImpl.java
@@ -0,0 +1,4 @@
+package com.OOCL.Todo.service.serviceImpl;
+
+public class TodoServiceImpl {
+}
